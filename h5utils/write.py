3fc333ec1ab53de0913e9c309a4584e7ec5af2c0
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2022-12-14 15:58:34 +0100

    write attributes to h5 group : complex objects are first pickled

--- h5utils/write.py
+++ h5utils/write.py
@@ -4,6 +4,8 @@
 
 # ====================================================
 # imports
+import pickle
+import numpy as np
 from h5py import Group
 
 from typing import Any
@@ -15,11 +17,15 @@ def write_attribute(group: Group,
                     name: str,
                     obj: Any) -> None:
     """Write a simple object as a H5 group attribute."""
-    group.attrs[name] = 'None' if obj is None else obj
+    try:
+        group.attrs[name] = 'None' if obj is None else obj
+
+    except TypeError:
+        group.attrs[name] = np.void(pickle.dumps(obj))
 
 
 def write_attributes(group: Group,
                      **kwargs: Any) -> None:
     """Write multiple object as h5 group attributes."""
-    for name, obj in kwargs.keys():
+    for name, obj in kwargs.items():
         write_attribute(group, name, obj)
