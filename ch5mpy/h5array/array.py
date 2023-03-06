fixed bugs in subsetting, new repeated_array for paired iteration through generally broadcastable arrays

ch5mpy/h5array/array.py
@@ -18,8 +18,8 @@ from ch5mpy._typing import NP_FUNC
 from ch5mpy._typing import SELECTOR
 
 from ch5mpy import Dataset
-from ch5mpy.h5array.chunks import ChunkIterator
-from ch5mpy.h5array.chunks import PairedChunkIterator
+from ch5mpy.h5array.chunks.iter import ChunkIterator
+from ch5mpy.h5array.chunks.iter import PairedChunkIterator
 from ch5mpy.h5array.indexing.selection import Selection
 from ch5mpy.h5array.io import read_one_from_dataset
 from ch5mpy.objects.dataset import DatasetWrapper
@@ -237,7 +237,7 @@ class H5Array(Generic[_T], numpy.lib.mixins.NDArrayOperatorsMixin):
 
     @property
     def size(self) -> int:
-        return self._dset.size
+        return int(np.prod(self.shape))
 
     # endregion
 

ch5mpy/h5array/chunks/iter.py
@@ -15,6 +15,8 @@ from typing import Generator
 from typing import TYPE_CHECKING
 
 import ch5mpy
+from ch5mpy.h5array.chunks.repeated_array import RepeatedArray
+from ch5mpy.h5array.chunks.utils import _as_valid_dtype
 from ch5mpy.h5array.indexing.slice import FullSlice
 from ch5mpy.h5array.indexing.slice import map_slice
 
@@ -113,13 +115,6 @@ def _get_chunk_indices(max_memory_usage: int | str,
     )
 
 
-def _valid_dtype(arr: npt.NDArray[Any], dtype: np.dtype[Any]) -> npt.NDArray[Any]:
-    if np.issubdtype(dtype, str):
-        return arr.astype(str)
-
-    return arr
-
-
 class ChunkIterator:
     def __init__(self,
                  array: H5Array[Any],
@@ -139,7 +134,7 @@ class ChunkIterator:
             self._array.read_direct(self._work_array, source_sel=map_slice(index), dest_sel=work_subset)
 
             # cast to str if needed
-            res = _valid_dtype(self._work_array, self._array.dtype)[work_subset]
+            res = _as_valid_dtype(self._work_array, self._array.dtype)[work_subset]
 
             # reshape to keep dimensions if needed
             if self._keepdims:
@@ -153,11 +148,10 @@ class PairedChunkIterator:
                  arr_1: H5Array[Any] | npt.NDArray[Any],
                  arr_2: H5Array[Any] | npt.NDArray[Any],
                  keepdims: bool = False):
-        if arr_1.shape != arr_2.shape:
-            raise ValueError(f'Cannot iterate chunks of arrays with different shapes: {arr_1.shape} != {arr_2.shape}')
+        broadcasted_shape = np.broadcast_shapes(arr_1.shape, arr_2.shape)
+        self._arr_1 = RepeatedArray(arr_1, broadcasted_shape)
+        self._arr_2 = RepeatedArray(arr_2, broadcasted_shape)
 
-        self._arr_1 = arr_1
-        self._arr_2 = arr_2
         self._keepdims = keepdims
 
         max_mem_1 = get_size(arr_1.MAX_MEM_USAGE) if isinstance(arr_1, ch5mpy.H5Array) else INF
@@ -166,31 +160,17 @@ class PairedChunkIterator:
         self._chunk_indices = _get_chunk_indices(min(max_mem_1, max_mem_2),
                                                  shape=arr_1.shape,
                                                  itemsize=max(arr_1.dtype.itemsize, arr_1.dtype.itemsize))
-        self._work_array_1 = get_work_array(arr_1.shape, self._chunk_indices[0], dtype=arr_1.dtype)
-        self._work_array_2 = get_work_array(arr_1.shape, self._chunk_indices[0], dtype=arr_2.dtype)
+        self._work_array_1 = get_work_array(broadcasted_shape, self._chunk_indices[0], dtype=arr_1.dtype)
+        self._work_array_2 = get_work_array(broadcasted_shape, self._chunk_indices[0], dtype=arr_2.dtype)
 
     def __repr__(self) -> str:
         return f"<PairedChunkIterator over 2 {self._arr_1.shape} H5Arrays>"
 
-    @staticmethod
-    def _read_array(arr: npt.NDArray[Any] | H5Array[Any],
-                    out: npt.NDArray[Any],
-                    source_sel: tuple[slice, ...],
-                    dest_sel: tuple[slice, ...]) -> None:
-        if isinstance(arr, ch5mpy.H5Array):
-            arr.read_direct(out, source_sel=source_sel, dest_sel=dest_sel)
-
-        else:
-            out[dest_sel] = arr[source_sel]
-
     def __iter__(self) -> Generator[tuple[tuple[FullSlice, ...], npt.NDArray[Any], npt.NDArray[Any]], None, None]:
         for index in self._chunk_indices:
             work_subset = map_slice(index, shift_to_zero=True)
-            self._read_array(self._arr_1, self._work_array_1, map_slice(index), work_subset)
-            self._read_array(self._arr_2, self._work_array_2, map_slice(index), work_subset)
-
-            res_1 = _valid_dtype(self._work_array_1, self._arr_1.dtype)[work_subset]
-            res_2 = _valid_dtype(self._work_array_2, self._arr_2.dtype)[work_subset]
+            res_1 = self._arr_1.read(self._work_array_1, map_slice(index), work_subset)
+            res_2 = self._arr_2.read(self._work_array_2, map_slice(index), work_subset)
 
             if self._keepdims:
                 res_1 = res_1.reshape((1,) * (self._arr_1.ndim - res_1.ndim) + res_1.shape)

ch5mpy/h5array/indexing/selection.py
@@ -46,7 +46,7 @@ def _cast_h5(obj: ListIndex | FullSlice) -> int | npt.NDArray[np.int_] | slice:
 def _gets_whole_dataset(index: SELECTOR | tuple[SELECTOR, ...]) -> bool:
     return (isinstance(index, tuple) and index == ()) or \
         (isinstance(index, slice) and index == slice(None)) or \
-        (is_sequence(index) and all((e is True for e in index)))
+        (is_sequence(index) and len(index) > 0 and all((e is True for e in index)))
 
 
 def _within_bounds(obj: ListIndex | FullSlice, shape: tuple[int, ...], shape_index: int) \
@@ -164,7 +164,7 @@ class Selection:
                         shape_index += 1
 
                 else:
-                    sel += _within_bounds(ListIndex(axis_index), shape, shape_index)
+                    sel += _within_bounds(ListIndex(axis_index.astype(np.int64)), shape, shape_index)
                     shape_index += 1
 
             else:
@@ -265,7 +265,7 @@ class Selection:
 
         return Selection([s for s in casted_selection if s is not None])                            # type: ignore[misc]
 
-    def iter_h5(self, array_ndim: int) -> Generator[
+    def iter_h5(self, array_shape: tuple[int, ...]) -> Generator[
         tuple[tuple[int | npt.NDArray[np.int_] | slice, ...],
               tuple[int | slice, ...]],
         None,
@@ -276,7 +276,10 @@ class Selection:
 
         if len(list_indices) == 0 or len(list_indices) == 1 and self._indices[list_indices[0]].ndim == 1:
             # make sure there are at least 2 ListIndex, over-wise the selection can simply be returned as is
-            yield self.get(), ()
+            dataset_sel = self.get()
+            loading_sel = tuple(0 for _ in takewhile(lambda x: x == 1, array_shape))
+
+            yield dataset_sel, loading_sel
             return
 
         whole_lists = [a.flatten() for a in np.broadcast_arrays(*(i for i in self._indices
@@ -289,7 +292,7 @@ class Selection:
                                 for i, e in enumerate(self) if not isinstance(e, NewAxisType))
 
             loading_sel = tuple(next(index_array) if i in list_indices else slice(None)
-                                for i, e in enumerate(islice(self, 0, array_ndim)))
+                                for i, e in enumerate(islice(self, 0, len(array_shape))))
 
             yield dataset_sel, loading_sel
 

ch5mpy/h5array/io.py
@@ -22,7 +22,7 @@ _DT = TypeVar("_DT", bound=np.generic)
 def read_from_dataset(dataset: Dataset[_DT] | DatasetWrapper[_DT],
                       selection: Selection,
                       loading_array: npt.NDArray[_DT]) -> None:
-    for dataset_idx, array_idx in selection.iter_h5(loading_array.ndim):
+    for dataset_idx, array_idx in selection.iter_h5(loading_array.shape):
         dataset.read_direct(loading_array, source_sel=dataset_idx, dest_sel=array_idx)
 
 
@@ -45,5 +45,5 @@ def write_to_dataset(dataset: Dataset[_DT] | DatasetWrapper[_DT],
     # we must make a copy here to ensure 'C' order
     values = values.copy()
 
-    for dataset_idx, array_idx in selection.iter_h5(values.ndim):
-        dataset.write_direct(values.squeeze(), source_sel=array_idx, dest_sel=dataset_idx)
+    for dataset_idx, array_idx in selection.iter_h5(values.shape):
+        dataset.write_direct(values, source_sel=array_idx, dest_sel=dataset_idx)